381572c4be1081512dbe65921b92f1c784b381b6
Sendexter Admin <senthil.exploring@gmail.com>
2017-08-28 08:26:47 +1000

    Included Angular parts, yet to resolve issues

--- routes/todo.js
+++ routes/todo.js
@@ -1,9 +1,41 @@
 var express = require('express');
+var mongojs = require('mongojs');
+
 var router = express.Router();
+var db = mongojs('mongodb://dexteruser:Qwerty123!@ds157723.mlab.com:57723/dexter', ["todo"]);
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/list', function(req, res, next){
+  db.todo.find(function(err, docs){
+    if(err != null){
+        console.log(err);
+    }
+    console.log(docs);
+    res.send(docs);
+  });
+});
+
+/* POST users listing. */
+router.post('/create', function(req, res, next){
+  db.todo.find(function(err, docs){
+    if(err){
+        console.log(err);
+        res.send(err);
+    }
+    console.log(docs);
+    res.send(docs);
+  });
+});
+
+/* DELETE users listing. */
+router.delete('/remove', function(req, res, next){
+  db.todo.find(function(err, docs){
+    if(err != null){
+        console.log(err);
+    }
+    console.log(docs);
+    res.send(docs);
+  });
 });
 
 module.exports = router;
